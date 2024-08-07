7c8ed278d3ccb236f7e1fe8f59a42ee6c0d33407
Educarv0 <160619368+Educarv0@users.noreply.github.com>
2024-08-06 22:18:32 -0300

    margin-bottom do container arrumada

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const menuMobile = document.querySelector(".menu-mobile");
-const menuLinks = document.querySelectorAll(".link");
-const menuIcon = document.querySelector(".hamburger");
-let menuAtivo = false;
+const navbar = document.getElementById("navbar");
 
-function openMenu() {
-    if (menuAtivo) {
-        closeMenu();
-        return;
+function changeNavbarBackground (position) {
+    if (position <= 0) {
+        return 'transparent';
     }
-    menuMobile.classList.add("active");
-    menuIcon.classList.add("fa-xmark");
-    menuLinks.forEach(el => {
-        el.classList.add("animation-links");
-    })
-    menuAtivo = true;
+    return 'rgb(12, 12, 12)';
 }
 
-function closeMenu() {
-    menuMobile.classList.remove("active");
-    menuIcon.classList.remove("fa-xmark");
-    menuLinks.forEach(el => {
-        el.classList.remove("animation-links");
-    })
-    menuAtivo = false;
-}
-
-menuLinks.forEach(element => {
-    element.addEventListener('click', () => {
-        closeMenu();
-    })
+window.addEventListener('scroll', function() {
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    navbar.style.backgroundColor = changeNavbarBackground(scrollPosition);
 });
