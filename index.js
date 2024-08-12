8140a7d19acb4d3e239544ecd9f29b6870c020ce
Educarv0 <160619368+Educarv0@users.noreply.github.com>
2024-08-12 19:03:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const navbar = document.getElementById("navbar");
+const menuMobile = document.querySelector(".menu-mobile");
+const menuLinks = document.querySelectorAll(".link");
+const hamburger = document.querySelector(".hamburger");
+let menuAtivo = false;
 
-function changeNavbarBackground (position) {
-    if (position <= 0) {
-        return 'transparent';
+function abrirMenu() {
+    if (menuAtivo) {
+        fecharMenu();
+        return;
     }
-    return 'rgb(12, 12, 12)';
+    menuMobile.classList.add("active");
+    hamburger.classList.add("fa-xmark");
+    menuLinks.forEach(el => {
+        el.classList.add("animation-links");
+    })
+    menuAtivo = true;
 }
 
-window.addEventListener('scroll', function() {
-    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
-    navbar.style.backgroundColor = changeNavbarBackground(scrollPosition);
+function fecharMenu() {
+    menuMobile.classList.remove("active");
+    hamburger.classList.remove("fa-xmark");
+    menuLinks.forEach(el => {
+        el.classList.remove("animation-links");
+    })
+    menuAtivo = false;
+}
+
+menuLinks.forEach(element => {
+    element.addEventListener('click', () => {
+        fecharMenu();
+    })
 });
